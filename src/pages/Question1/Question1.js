0d49c1c796d92c45ddc1a59c447401c0c3a155f7
flavie25 <flavie.dussaillant@gmail.com>
2022-02-11 16:51:57 +0100

    button function

--- src/pages/Question1/Question1.js
+++ src/pages/Question1/Question1.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import clsx from 'clsx';
+import React, { useState } from 'react';
 import Button from '../../components/button';
 import Player from '../../components/player/Player';
 import style from './question.module.css';
 
 const Question1 = () => {
-  const wrong = () =>{
-    
-    console.log("loupé");
-    alert("dommage");
-    
+  const [firstAnswer, setFirstAnswer] = useState(true);
+  const [secondAnswer, setSecondAnswer] = useState(true);
+  
+  const firstWrong = () =>{
+    setFirstAnswer(false);
+    // console.log("loupé");
+    // alert("dommage");  
+  }
+  const secondWrong = () =>{
+    setSecondAnswer(false);
+    // console.log("loupé");
+    // alert("dommage");  
   }
   const right = () => {
     console.log("gg");
@@ -17,9 +25,9 @@ const Question1 = () => {
     <div className={style.question}>
       <Player />
       <p>Q01</p>
-      <Button buttonName="réponse 1" onClick={wrong}/>
-      <Button buttonName="réponse 2" onClick={wrong}/>
-      <Button buttonName="réponse 3" onClick={right}/>
+      <button buttonName="réponse 1" className={clsx(style.button, {[style.buttonWrong]: !firstAnswer})} onClick={firstWrong}>Réponse 1</button>
+      <button buttonName="réponse 1" className={clsx(style.button, {[style.buttonWrong]: !secondAnswer})} onClick={secondWrong}>Réponse 2</button>
+      <button buttonName="réponse 1" className={style.button} onClick={right}>Réponse 3</button>
     </div>
   );
 };
