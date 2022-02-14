0c655b8d66dff52b0fc51afab75da2ad137f1b2f
paule juoilde <guillaumemengelatte@hotmail.fr>
2022-02-14 09:59:14 +0100

    font family

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import Player from '../../components/player/Player'
 import style from './index.module.css'
 import { Link } from 'react-router-dom'
 
 
 const Home = () =>{
-
+    
     return (
         <div className={style.home}>
             <h1>Hello</h1>
             <p>kjfhjhkzgrqghrehghroghroge</p>
-            <Player />
             <Link to="/question" className="">question</Link>
         </div>      
     )
