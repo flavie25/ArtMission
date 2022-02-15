1bcafbe973c8e27378c179a59352a284ae8a0ce8
paule juoilde <guillaumemengelatte@hotmail.fr>
2022-02-15 15:02:19 +0100

    corection questions css

--- src/pages/Questions/Question3.js
+++ src/pages/Questions/Question3.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Question from '../../components/question/question';
-import style from './questions.module.css'
+import './questions.css';
 
 const Question3 = () => {
   return (
-    <div className={style.question3}>
+    <div className="question3">
         <Question numQuestion={3} numScreen={5}/>
     </div>
   );
