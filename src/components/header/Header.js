ec06005393c5c7c556df6684de8955332c58fa12
flavie25 <flavie.dussaillant@gmail.com>
2022-02-15 10:08:44 +0100

    animation lottie

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,21 +3,23 @@ import lottie from 'lottie-web';
 import animation from '../../Assets/Top_Art_Mission.json'
 import style from './header.module.css'
 
-const header = () => {
-  let Lanimation = () => {lottie.loadAnimation({
-    container: document.querySelector('#lottie-player'),
-    animationData: animation,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-  });
+const Header = () => {
+  React.useEffect(() =>{
+    lottie.loadAnimation({
+      container: document.querySelector('#lottie-player'),
+      animationData: animation,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+    });
+   }) 
   console.log("oui");
-  }
+  
 
   return (
-    <div id="lottie-player" onLoad={Lanimation()} className={style.lottiePlayer}> 
+    <div id="lottie-player"  className={style.lottiePlayer}> 
     </div>
   );
 };
 
-export default header;
+export default Header;
