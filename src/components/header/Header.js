86c48e4963be4f150e0dc7f302088ac99db516d2
flavie25 <flavie.dussaillant@gmail.com>
2022-02-17 16:14:05 +0100

    v12.1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,9 +4,6 @@ import animationA from '../../Assets/Top_Art_Mission.json'
 import animationB from '../../Assets/Top_Art_Mission_2.json'
 import style from './header.module.css'
 import Player from '../../components/audioPlayer'
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-// import App from '../../App/index'
 
 const Header = ({screen, speakerId}) => {
   const animationSpeaker = speakerId? animationA : animationB
@@ -19,26 +16,6 @@ const Header = ({screen, speakerId}) => {
       autoplay: true,
     });
    }) 
-
-  //  const scroll = () =>{
-  //       document.getElementById('footer').scrollIntoView({ behavior: 'smooth', block: 'end' });
-  //     }
-  // const location = useLocation();
-  // useEffect(() => {
-  //   const delayeScroll = () => {
-  //     window.setTimeout(scroll, 5000);
-  //   }
-  //   delayeScroll()
-  // }, [location]);
-
-  // useEffect(() => {
-  //   const scroll = () =>{
-  //     document.getElementById('footer').scrollIntoView({ behavior: 'smooth', block: 'end' });
-  //     // window.requestAnimationFrame(scroll())
-  //   }
-  //   scroll()
-  //   console.log('oui'); 
-  // }, []);
 
   return (
     <div className={style.header}>
